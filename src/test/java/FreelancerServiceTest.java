98e49401dc5eb81a6199262565374e2586cba0cc
NTimaeva <167666766+NTimaeva@users.noreply.github.com>
2024-07-22 17:46:52 +0300

    Update FreelancerServiceTest.java

--- src/test/java/FreelancerServiceTest.java
+++ src/test/java/FreelancerServiceTest.java
@@ -1,46 +1,34 @@
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import ru.netology.services.FreelancerService;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class FreelancerServiceTest {
+  @Test
+  public void testCase1() {
+    FreelancerService service = new FreelancerService();
 
-    @Test
-    public void testCase1 ()
-    {
+    int income = 0;
+    int expenses = 0;
+    int threshold = 0;
 
-        FreelancerService service = new FreelancerService();
+    int actual = service.calculate(10_000, 3_000, 20_000);
+    int expected = 3;
 
-        int income =0;
-        int expenses=0;
-        int threshold=0;
+    Assertions.assertEquals(expected, actual);
+  }
+  @Test
+  public void testCase2() {
+    FreelancerService service = new FreelancerService();
 
+    int income = 0;
+    int expenses = 0;
+    int threshold = 0;
 
-        int actual = service.calculate (10_000, 3_000, 20_000);
-        int expected = 3;
+    int actual = service.calculate(100_000, 60_000, 150_000);
+    int expected = 2;
 
-        Assertions.assertEquals(expected, actual);
-
-
-    }
-    @Test
-    public void testCase2 ()
-    {
-
-        FreelancerService service = new FreelancerService();
-
-        int income =0;
-        int expenses=0;
-        int threshold=0;
-
-
-        int actual = service.calculate (100_000, 60_000, 150_000);
-        int expected = 2;
-
-        Assertions.assertEquals(expected, actual);
-
-
-    }
-
+    Assertions.assertEquals(expected, actual);
+  }
 }
